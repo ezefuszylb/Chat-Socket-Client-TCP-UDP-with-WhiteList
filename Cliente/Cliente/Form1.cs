03c7ce42e8c8b899e8edb643ad837e81708b8646
Ezequiel Fusco <eze.fus.zylb@gmail.com>
2022-11-27 23:57:58 -0300

    Primer commit

---
+++ Cliente/Cliente/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Transitions;
+
+namespace Cliente
+{    
+
+    public partial class Form1 : Form
+    {
+        static private NetworkStream stream;
+        static private StreamWriter streamw;//variables de lectura y escritura
+        static private StreamReader streamr;
+        static private TcpClient client = new TcpClient();//nos permite hacer contacto entre clientes y servidor
+        static private string nombre = "unknown";//identifico al usuario
+
+        private delegate void DaddItem(String s);
+
+        private void AddItem(String s)
+        {
+            listBox1.Items.Add(s);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        void Listen()
+        {
+            while(client.Connected)//mientras el cliente este conectado
+            {
+                try
+                {
+                    this.Invoke(new DaddItem(AddItem), streamr.ReadLine());//lee el servidor 
+                }
+                catch//si no esta abierto el servidor
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");//pasa esto y se cierra
+                    Application.Exit();
+                }
+            }
+        }
+
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);//se conecta a esa ip y soket
+                if(client.Connected)//si se conecta
+                {
+                    Thread t = new Thread(Listen);//creo el objeto que escuche
+
+                    stream = client.GetStream();
+                    streamw = new StreamWriter(stream);//y el cliente lee y escribe
+                    streamr = new StreamReader(stream);
+
+
+                    streamw.WriteLine(nombre);
+                    streamw.Flush();
+
+                    t.Start();
+                }
+                else
+                {//si no se conecta
+                    MessageBox.Show("Servidor no Disponible"); //una cajita de mensaje
+                }
+            }
+            catch(Exception ex)
+            {//si el servidr no se prendio
+                MessageBox.Show("Servidor no Disponible");
+                Application.Exit();
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            nombre = txtUsuario.Text;
+            Conectar();
+            Transition t = new Transition(new TransitionType_EaseInEaseOut(900));
+            t.add(lbTitulo1, "Left", 555);
+            t.add(txtUsuario, "Left", 555);
+            t.add(btnConectar, "Left", 555);
+            t.add(listBox1, "Left", 26);
+            t.add(txtMensaje, "Left", 26);
+            t.add(btnEnviar, "Left", 283);
+            t.run();
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            IPEndPoint ipep = (IPEndPoint)client.Client.RemoteEndPoint;
+            IPAddress ipa = ipep.Address;
+            streamw.WriteLine(" (" + ipa + ") = " + txtMensaje.Text);
+            streamw.Flush();
+            txtMensaje.Clear();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnEnviar.Location = new Point(-329, 250);
+            txtMensaje.Location = new Point(-329, 250);
+            listBox1.Location = new Point(-329, 23);
+        }
+    }
+}
---
+++ Servidor/Servidor/Servidor_Chat.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Servidor
+{/// <summary>
+/// como crear un servidor al que se pueden conectar
+/// </summary>
+    class Servidor_Chat
+    {
+        /// <summary>
+        /// crea el server, que espera la conxion del cliente, y cliente
+        /// </summary>
+        private TcpListener server;
+        private TcpClient client = new TcpClient();
+        /// <summary>
+        /// establece el ip y el puerto
+        /// </summary>
+        private IPEndPoint ipendpoint = new IPEndPoint(IPAddress.Any, 8000);
+        /// <summary>
+        /// pa que se haga la conexion y que se vea la info
+        /// </summary>
+        private List<conexion> list = new List<conexion>();
+
+        conexion con; //creo con la conexion con la estructura de abajo
+
+        private struct conexion
+        {
+            /// <summary>
+            /// creo la estructura de mi conexion que despues uso
+            /// </summary>
+            public NetworkStream stream;
+            public StreamWriter streamw;
+            public StreamReader streamr;
+            public string nombre;
+        }
+
+        public bool validarWhiteList(string ip, string whitelist)
+        {
+            string line = string.Empty;
+            StreamReader streamrw = new StreamReader(whitelist);
+
+            while(line != null)
+            {
+                line = streamrw.ReadLine();
+                if(ip == line)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public Servidor_Chat()
+        {
+            Inicio();//el svr se incia haciendo lo de abajo
+        }
+        public void Inicio()
+        {
+            Console.WriteLine("Servidor Activado!");
+            
+            server = new TcpListener(ipendpoint);//que escuche con la ip y puerto que establecimos antes
+            server.Start();
+
+
+            while(true)
+            {
+                client = server.AcceptTcpClient();//el servidor acepta al cliente que creamos antes
+                IPEndPoint ipep = (IPEndPoint)client.Client.RemoteEndPoint;
+                IPAddress ipa = ipep.Address;                
+                if(validarWhiteList(ipa.ToString(), "C:/Users/Eze/Desktop/Chat/Servidor/Servidor/bin/Debug/whitelist.txt"))
+                {
+                    con = new conexion();//se crea la conexion
+                    con.stream = client.GetStream();//el cliente hace la conexion con el servidor
+                    con.streamr = new StreamReader(con.stream);//el server lee 
+                    con.streamw = new StreamWriter(con.stream);// y escribe
+                    con.nombre = con.streamr.ReadLine();//el otro lee y capta lo que enviaron
+
+                    list.Add(con);//y agrega al listado de la conexion que hicimos antes
+
+                    Console.WriteLine(con.nombre + " se a Conectado.");
+                    Console.WriteLine(ipa + " se a Conectado.");
+
+                    Thread escuchaCon = new Thread(Escuchar_conexion);
+                    escuchaCon.Start();//empieza a escuchar la conexion
+                }
+                else
+                {
+                    Console.WriteLine("El cliente no esta dentro de la whitelist");
+                }
+                
+            }
+        }
+        void Escuchar_conexion()
+        {
+
+            conexion hcon = con;//una variable con la estructura conexion
+
+            do
+            {
+                try
+                {
+                    string mensaje = hcon.streamr.ReadLine();//se verifica si se hizo la conexion, y se lee el mensaje que se envia
+                    Console.WriteLine(hcon.nombre + "" + mensaje);
+                    foreach (conexion c in list)
+                    {
+                        try
+                        {
+                            c.streamw.WriteLine(hcon.nombre + "" + mensaje);//se escribe el mensaje que se envia
+                            c.streamw.Flush();
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+                catch
+                {
+                    list.Remove(hcon);//si se cierra el usuario se borrra los mensajes
+                    Console.WriteLine(con.nombre + " se a Desconectado.");
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
+
+
+
